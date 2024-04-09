9bc6d0924c719a9039429b6cf865ede92bbdeb73
chiranz <chiranpoudyal@gmail.com>
2024-04-09 21:19:29 +0530

    script: add deploy script

--- scripts/Deployer.s.sol
+++ scripts/Deployer.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import "forge-std/Script.sol";
 import {GoatV1Factory} from "../contracts/exchange/GoatV1Factory.sol";
 import {GoatV1Router} from "../contracts/periphery/GoatRouterV1.sol";
+import {TokenFactory} from "../contracts/tokens/TokenFactory.sol";
 
 import {console2} from "forge-std/Test.sol";
 
@@ -11,12 +12,14 @@ contract DeployMainnet is Script {
     address private _WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY1");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         GoatV1Factory factory = new GoatV1Factory(_WETH);
         GoatV1Router router = new GoatV1Router(address(factory), _WETH);
+        TokenFactory tokenFactory = new TokenFactory(address(factory), _WETH);
         console2.log("Factory address: ", address(factory));
         console2.log("Router address: ", address(router));
+        console2.log("TokenFactory address: ", address(tokenFactory));
         vm.stopBroadcast();
     }
 }
